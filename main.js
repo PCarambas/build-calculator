cff6ff675cc08b6767a72210a58354568366252a
Peggy Carambas <pcarambas@gmail.com>
2022-09-09 13:17:22 -0500

    First commit of build-calculator

---
+++ main.js
@@ -0,0 +1,63 @@
+// Write a subtraction function
+
+const subtract = (num1, num2) => {
+    const sum = num1 - num2
+
+    return sum
+}
+
+let result = subtract(10, 5)
+// console.log(result)
+
+
+// Write a division function
+
+const divide = (num1, num2) => {
+    const sum = num1 / num2
+
+    return sum
+}
+
+let outcome = divide(14, 2)
+// console.log(outcome)
+
+
+// Write a multiplication function
+
+const multiply = (num1, num2) => {
+    const sum = num1 * num2
+
+    return sum
+}
+
+let effect = multiply(4, 4)
+// console.log(effect)
+
+
+// Write square function
+
+const square = (num) => {
+    const sum = num * num
+
+    return sum
+}
+
+let number = square(3)
+// console.log(number)
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
